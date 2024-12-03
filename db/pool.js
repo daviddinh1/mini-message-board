b132ea2213838d00ab43e105e938b765ccb3e19a
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-12-02 19:56:48 -0600

    changing pool to connect o railway

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+// module.exports = new Pool({
+//   host: "localhost", // or wherever the db is hosted
+//   user: process.env.ROLE_NAME,
+//   database: "msgs",
+//   password: process.env.ROLE_PASS,
+//   port: 5432, // The default port
+// });
+
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: process.env.ROLE_NAME,
-  database: "msgs",
-  password: process.env.ROLE_PASS,
-  port: 5432, // The default port
+  connectionString:
+    "postgresql://postgres:bzlUNEefGDtLUWjzgAhzHgUUyoGzFQcb@junction.proxy.rlwy.net:58126/railway,",
 });
